77a8586a55265f05b9e75f3b6f8a8b326d5d662b
sukhoeing <sukhoeing@gmail.com>
2013-01-06 13:10:56 +0000

    // LA5095    Transportation // 陈锋

---
+++ TrainingGuide/exercises/ch5/LA5095.cc
@@ -0,0 +1,106 @@
+// LA5095    Transportation
+// 陈锋
+#include<cstring>
+#include<cstdio>
+#include<string>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int MAXN = 100 + 10, INF = 1000000000;
+typedef long long Int64;
+
+struct Edge {
+  int from, to, cap, flow, cost;
+};
+
+template<int MAXSIZE>
+struct MCMF {
+  int n, m;
+  vector<Edge> edges;
+  vector<int> G[MAXSIZE];
+  bool inq[MAXSIZE];         // 是否在队列中
+  int d[MAXSIZE];           // Bellman-Ford
+  int p[MAXSIZE];           // 上一条弧
+  int a[MAXSIZE];           // 可改进量
+
+  void init(int n) {
+    this->n = n;
+    for(int i = 0; i < n; i++) G[i].clear();
+    edges.clear();
+  }
+
+  void AddEdge(int from, int to, int cap, int cost) {
+    edges.push_back((Edge){from, to, cap, 0, cost});
+    edges.push_back((Edge){to, from, 0, 0, -cost});
+    m = edges.size();
+    G[from].push_back(m-2);
+    G[to].push_back(m-1);
+  }
+
+  bool BellmanFord(int s, int t, int &flow, Int64& ans) {
+    fill_n(d, n, INF);
+    fill_n(inq, n, false);
+    d[s] = 0; inq[s] = true; p[s] = 0; a[s] = INF;
+
+    queue<int> Q;
+    Q.push(s);
+    while(!Q.empty()) {
+      int u = Q.front(); Q.pop();
+      inq[u] = false;
+      for(int i = 0; i < G[u].size(); i++) {
+        Edge& e = edges[G[u][i]];
+        if(e.cap > e.flow && d[e.to] > d[u] + e.cost) {
+          d[e.to] = d[u] + e.cost;
+          p[e.to] = G[u][i];
+          a[e.to] = min(a[u], e.cap - e.flow);
+          if(!inq[e.to]) { Q.push(e.to); inq[e.to] = true; }
+        }
+      }
+    }
+    if(d[t] == INF) return false; //s-t 不连通，失败退出  
+    flow += a[t];
+    ans += (Int64)d[t] * (Int64)a[t];
+    int u = t;
+    while(u != s) {
+      edges[p[u]].flow += a[t];
+      edges[p[u]^1].flow -= a[t];
+      u = edges[p[u]].from;      
+    }
+    return true;
+  }
+
+  // 需要保证初始网络中没有负权圈
+  Int64 Mincost(int s, int t, int& flow) {
+    Int64 ans = 0;
+    while(BellmanFord(s, t, flow, ans));
+    return ans;
+  }
+};
+
+MCMF<MAXN> solver;
+int K, M, N;
+void runcase() {
+    solver.init(N + 1);
+    
+    int u, v, a, cap;
+    for(int i = 1; i <= M; i++)
+    {
+        scanf("%d%d%d%d", &u, &v, &a, &cap);
+        for(int c = 0; c < cap; c++)
+            solver.AddEdge(u, v, 1, (2*c + 1) * a);
+    }
+    
+    solver.AddEdge(0, 1, K, 0);
+    int flow = 0;
+    Int64 cost = solver.Mincost(0, N, flow);
+    if(flow < K) cost = -1;
+    printf("%lld\n", cost);
+}
+
+int main() {
+    while(scanf("%d%d%d", &N, &M, &K) == 3)
+        runcase();
+    return 0;
+}
