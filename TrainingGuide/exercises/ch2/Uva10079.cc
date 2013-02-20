612f3a5cd84ee050476c952f3eb92c67453bb1af
sukhoeing <sukhoeing@gmail.com>
2013-02-20 10:02:35 +0000

    Uva10079.cc

---
+++ TrainingGuide/exercises/ch2/Uva10079.cc
@@ -0,0 +1,24 @@
+// UVa10079 Pizza Cutting
+// 陈锋
+#include<cstring>
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+long long N;
+void runcase()
+{        
+    // 注意F(1) == 2
+    cout<<N * (N+1) / 2 + 1<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    while(true)
+    {
+        cin>>N;
+        if(N < 0) break;
+        runcase();
+    }
+    return 0;
+}
